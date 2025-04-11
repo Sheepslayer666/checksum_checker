bf78d30fd2c3608269ea6c8ad27284a789559610
Sheepslayer666 <48016217+Sheepslayer666@users.noreply.github.com>
2025-04-11 12:43:15 +0200

    Add files via upload
    simplified code

--- checksum.py
+++ checksum.py
@@ -1,11 +1,8 @@
 import hashlib
 
-print('Filame:')
-file_name = input()
-print('Original md5:')
-original_md5 = input()
-print('Original sha256:')
-original_sha256 = input()
+file_name = input("Filename: ")
+original_md5 = input("Original md5:")
+original_sha256 = input("Original sha256: ")
 
 with open(file_name, 'rb') as file_to_check:
     data = file_to_check.read()    
@@ -13,7 +10,7 @@ with open(file_name, 'rb') as file_to_check:
 if original_md5 == md5_returned:
     print("MD5 OK")
 else:
-    print("MD5 NOK")
+    print("MD5 not okay")
 
 with open(file_name, 'rb') as file_to_check:
     data = file_to_check.read()    
@@ -21,4 +18,4 @@ with open(file_name, 'rb') as file_to_check:
 if original_sha256 == sha256_returned:
     print("sha256 OK")
 else:
-    print("sha256 NOK")
+    print("sha256 not okay")
